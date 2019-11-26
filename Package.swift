cb60e10a0f67cde00750c38525e294ac9688130c
Blair McArthur <blair.mcarthur@icloud.com>
2019-11-26 15:27:44 -0500

    switching Fluent-Kit dependency to master

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
     ],
     targets: [
